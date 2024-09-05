41294e416ef09d2b066e8bb558e107d9f393c572
yaswanth reddy Narapa reddy <yaswanthreddy.narapareddy@aveva.com>
2024-09-05 11:50:20 +0530

    creating mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HydraApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
