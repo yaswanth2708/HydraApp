75d9ef8adc797297eb10dda862e97873522b2944
Siddhesh Bahety <siddheshbahety@gmail.com>
2024-09-05 15:35:25 +0530

    admin side

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HydraApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult AdminLogin()
+        {
+            return View();
+        }
+        public IActionResult AdminHomePage()
+        {
+            return View();
+        }
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}
